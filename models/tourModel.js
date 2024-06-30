104f315289e4d665be44493db16807d2469176c3
Burak Karaman <karaman.buraak@gmail.com>
2024-06-30 20:40:15 +0200

    Add new fields to tourSchema in tourModel.js

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,15 +6,52 @@ const tourSchema = new mongoose.Schema(
       type: String,
       required: [true, "A tour must have a name"],
       unique: true,
+      trim: true,
       maxlength: [50, "Name must be less than or equal to 50 characters"],
     },
-    rating: {
+    duration: {
+      type: Number,
+      required: [true, "A tour must have a duration"],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, "A tour must have a duration"],
+    },
+    difficulty: {
+      type: String,
+      required: [true, "A tour must have a difficulty field"],
+    },
+    ratingsAverage: {
       type: Number,
       default: 4.5,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
     },
     price: {
       type: Number,
       required: [true, "A tour must have a price"],
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, "A tour must have a summary."],
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: [true, "A tour must have a description"],
+    },
+    imageCover: {
+      type: String,
+      required: [true, "A tour mus have a cover image"],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
     },
   },
   {
