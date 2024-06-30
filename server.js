f45ca6b2b8fd57e645e0c9e1d69360a5bee47cbc
Burak Karaman <karaman.buraak@gmail.com>
2024-06-30 14:11:24 +0200

    Add mongoose dependency and connect to MongoDB in server.js

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+const mongoose = require("mongoose");
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 const app = require("./app");
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => console.log("DB Connection"))
+  .catch((err) => console.log(err));
 
 //* START SERVER
 app.listen(PORT, () => {
