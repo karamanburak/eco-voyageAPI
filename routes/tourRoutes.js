deb72b9c653f3f1967ed4ded7be633dd2e564b30
Burak Karaman <karaman.buraak@gmail.com>
2024-07-02 00:05:43 +0200

    Add getTourStats and getMonthlyPlan functions to tourController and corresponding routes to tourRoutes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -8,6 +8,9 @@ router
   .route("/top-5-cheap")
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
+router.route("/tour-stats").get(tourController.getTourStats);
+router.route("/monthly-plan").get(tourController.getMonthlyPlan);
+
 router
   .route("/")
   .get(tourController.getAllTours)
