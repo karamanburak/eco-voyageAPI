c61eb4637633caf53f917c37381c0cf0859b8231
Burak Karaman <karaman.buraak@gmail.com>
2024-07-13 11:31:07 +0200

    Add role field to user creation, modifiy tour model to include guides array, and update mongoose connection in server.js

--- server.js
+++ server.js
@@ -18,12 +18,7 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("DB connection successful!"));
+mongoose.connect(DB).then(() => console.log("DB connection successful!"));
 
 const port = process.env.PORT || 8000;
 const server = app.listen(port, () => {
